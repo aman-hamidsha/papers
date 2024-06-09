8e222f4edfd0fa1948736d4c65f3059a67277090
go-hickey <stargrasstv@gmail.com>
2024-06-09 23:11:48 +0100

    2014 doing

--- 2014.hs
+++ 2014.hs
@@ -5,5 +5,25 @@
 length' :: [a] -> Int
 length' = foldr (\x acc -> acc + 1) 0 
 
-binary' :: Int -> [Char]
-binary' n = foldr (\) "" n
+-- b
+
+
+-- ASK 
+-- binary' :: Int -> [Char]
+-- binary' 1 = "1"
+-- binary n = 
+
+
+-- c
+
+fac 0 = 1
+fac n = n * fac(n-1)
+
+e :: Float -> Int -> Float
+e flt n = sum $ map (\x -> flt^x / fac (fromIntegral x)) [0..(fromIntegral n)]
+
+
+-- 3
+-- a
+
+isort :: Ord a => [a] -> [a]
