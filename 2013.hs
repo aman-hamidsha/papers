9e1e3ae27492053cc55f4365719fc33015887739
go-hickey <stargrasstv@gmail.com>
2024-06-09 21:46:34 +0100

    2013 almsot done

--- 2013.hs
+++ 2013.hs
@@ -15,3 +15,61 @@ isPrime n = not $ foldr (\x acc -> n `mod` x == 0 || acc) False [2..n-1]
 
 
 -- c
+anPlusOne :: Float -> Float -> Float
+anPlusOne an x = (an+(x/an))/2
+
+nsqrt :: Float -> Float 
+nsqrt x = foldr anPlusOne x [1..10]
+
+
+-- q3
+
+-- a
+charPairs = zip ['a'..'z'] [1..26]
+ord :: Char -> Int
+ord c = snd $ head $ filter (\(x,y) -> x == c) charPairs
+
+-- b (ASK)
+
+
+-- primes :: Int -> [Int]
+-- primes 2 = [2]
+-- primes (x:xs) = filter ()
+
+
+-- q4 (ASK) 
+
+data List a = Null | Cons a (List a) deriving Show
+
+-- a
+head' :: List a -> a
+head' (Cons x _) = x
+head' Null = error "Empty List"
+
+
+-- b 
+tail' :: List a -> List a
+tail' (Cons x xs) = xs
+tail' Null = Null
+
+
+-- c
+append' :: List a -> List a -> List a
+append' Null ys = ys
+append' (Cons x xs) ys = Cons x (append' xs ys)
+
+-- append' :: List a -> List a -> List a
+-- append' xs ys = foldr Cons ys xs
+
+-- d
+drop' :: Int -> List a -> List a 
+drop' 0 l = l
+drop' 1 l = l 
+drop' n Null = Null
+drop' n (Cons _ xs) = drop' (n - 1) xs
+
+
+-- 5
+
+
+-- a
