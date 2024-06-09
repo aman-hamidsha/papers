b155f24af7dc56633cdec44ce55ddd093a5f1644
go-hickey <stargrasstv@gmail.com>
2024-06-09 22:52:11 +0100

    2013 done

--- 2013.hs
+++ 2013.hs
@@ -6,7 +6,7 @@ sumsq n = foldr (\x acc -> x^2 + acc) 0 [1..n]
 
 
 -- b
-isPrime :: Int -> Bool 
+isPrime :: Int -> Bool
 isPrime 0 = False
 isPrime 1 = False
 isPrime 2 = True
@@ -18,7 +18,7 @@ isPrime n = not $ foldr (\x acc -> n `mod` x == 0 || acc) False [2..n-1]
 anPlusOne :: Float -> Float -> Float
 anPlusOne an x = (an+(x/an))/2
 
-nsqrt :: Float -> Float 
+nsqrt :: Float -> Float
 nsqrt x = foldr anPlusOne x [1..10]
 
 
@@ -62,9 +62,9 @@ append' (Cons x xs) ys = Cons x (append' xs ys)
 -- append' xs ys = foldr Cons ys xs
 
 -- d
-drop' :: Int -> List a -> List a 
+drop' :: Int -> List a -> List a
 drop' 0 l = l
-drop' 1 l = l 
+drop' 1 l = l
 drop' n Null = Null
 drop' n (Cons _ xs) = drop' (n - 1) xs
 
@@ -82,7 +82,7 @@ pow :: Float -> Int -> Float  -- pow 2.0 3 => 8.0
 pow a n = for 1 (n-1) a (a*)
 
 
-sumints :: Int -> Int 
+sumints :: Int -> Int
 sumints n = error "not implemented"
 
 
@@ -92,3 +92,67 @@ repeatuntil s p f | p s = s
 
 log2 :: Float -> Float
 log2 n = repeatuntil n (< 2) (/2)
+
+
+--  6 
+
+
+-- a
+-- foldr :: ()
+concat' :: [[a]] -> [a]
+concat'  = foldr (++) []
+
+--b
+map' :: (a -> b) -> [a] -> [b]
+map' f ls = foldr (\x accList -> [(f x)] ++ accList) [] ls
+
+-- map' f = foldr (\x accList -> (f x) : accList) []
+
+-- c
+length' :: [a] -> Int
+length' = foldl (\acc x -> 1 + acc) 0  
+
+-- d
+reverse' :: [a] -> [a]
+reverse' ls = foldl (\acc x -> [x] ++ acc ) [] ls
+
+-- result of thing in brackets will be new acc
+
+{-
+Iteration 1:
+Element: 1
+Accumulator (acc): []
+Function Application: (\acc x -> [x] ++ acc) [] 1
+Result: [1] ++ [] which is [1]
+New Accumulator: [1]
+Iteration 2:
+Element: 2
+Accumulator (acc): [1]
+Function Application: (\acc x -> [x] ++ acc) [1] 2
+Result: [2] ++ [1] which is [2, 1]
+New Accumulator: [2, 1]
+Iteration 3:
+Element: 3
+Accumulator (acc): [2, 1]
+Function Application: (\acc x -> [x] ++ acc) [2, 1] 3
+Result: [3] ++ [2, 1] which is [3, 2, 1]
+New Accumulator: [3, 2, 1]
+Iteration 4:
+Element: 4
+Accumulator (acc): [3, 2, 1]
+Function Application: (\acc x -> [x] ++ acc) [3, 2, 1] 4
+Result: [4] ++ [3, 2, 1] which is [4, 3, 2, 1]
+New Accumulator: [4, 3, 2, 1]
+After all iterations are complete, the final value of the accumulator is [4, 3, 2, 1], which is the reversed list.
+
+Summary:
+Initial list: [1, 2, 3, 4]
+After folding:
+[1]
+[2, 1]
+[3, 2, 1]
+[4, 3, 2, 1]
+Final reversed list: [4, 3, 2, 1]
+
+
+-}
