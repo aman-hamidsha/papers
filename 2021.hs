dfb23e2bd20e9346edefd36d571e5a184520e173
go-hickey <stargrasstv@gmail.com>
2024-06-10 13:51:31 +0100

    23123

--- 2021.hs
+++ 2021.hs
@@ -59,3 +59,42 @@ concatMap' f  = foldr (\x acc -> f x ++ acc ) []
 -- ASK 
 concatMapM' :: Applicative f => (a -> f [b]) -> [a] -> f [b]
 concatMapM' f  = foldr (\x acc -> (++) <$> f x <*> acc) (pure [])
+
+
+
+
+-- 2
+
+
+
+-- a
+
+ofSize :: Int -> [a] -> [[a]]
+ofSize n ls = map (\(x,y,z) -> [x,y,z]) [(a,b,c) | a <- ls, b <- ls, c <- ls]
+
+
+
+
+
+
+
+
+
+-- 3
+--a 
+-- i 
+data Layout a = Element a
+    | Vertical (Layout a) (Layout a)
+    | Horizontal (Layout a) (Layout a)
+
+instance Functor Layout where 
+    fmap :: (a -> b) -> Layout a -> Layout b
+    fmap f (Element x) = Element (f x)
+    fmap f (Vertical l1 l2) = Vertical (fmap f l1) (fmap f l2)
+    fmap f (Horizontal l1 l2) = Horizontal (fmap f l1) (fmap f l2)
+
+
+instance Foldable Layout where
+    foldr :: (a -> b -> b) -> b -> Layout a -> b
+    foldr f acc (Element x) = f x acc 
+    foldr f acc (Vertical l1 l2) =   (foldr f acc l1) <> (foldr f acc l2)
